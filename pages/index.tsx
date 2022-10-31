ffe6094cfd2f07995df58dbaf77d9f574bf9ecf5
Marco Cabrera <52000890+marcocabrerahdez@users.noreply.github.com>
2022-10-31 19:49:17 +0000

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { GetStaticProps } from 'next'
+import fetch from 'isomorphic-unfetch'
 import Layout from '@components/Layout/Layout'
 import MyHeader from '@components/Header/Header'
 import FileLoader from '@components/FileLoader/FileLoader'
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await fetch('https://gco-project.vercel.app/api/recommendation')
+  const { data: recommendationList }: TAPIRecommendationResponse = await response.json()
+
+  return {
+    props: {
+      recommendationList,
+    },
+  }
+}
+
 
 const Home = () => {
   return (
